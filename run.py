ad6e2cfb993aaab9816ab4f5ac56bd6ace1cc158
CatherineP37 <>
2023-10-11 20:12:42 +0000

    Add functions

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
+def add(n1, n2):
+    n1 + n2
+
+def subtract(n1, n2):
+    n1 - n2
+
+def multiply(n1, n2):
+    n1 * n2
+
+def divide(n1, n2):
+    n1 / n2
+
+
 number_1 = int(input("Enter a number: "))
 
 operator = input("Choose an operator (+, -, *, /) ")
 
 number_2 = int(input("Enter another number: "))
+
+
