821a5f4f313d2f391baff6a09c5b8ec30e95f982
CatherineP37 <>
2023-10-12 11:24:30 +0000

    Add comments

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 
+"""
+This is a while loop to run code while the user wants to do a calculation.
 
+"""
 
 another_calculation = True
 
 while another_calculation is True:
+
+    """
+    These are the calculation functions.
+
+    """
     def add(n1, n2):
         n1 + n2
 
@@ -16,11 +24,21 @@ while another_calculation is True:
     def divide(n1, n2):
         n1 / n2
 
+    """
+    This is the where the user enters the information for the calculation.
+
+    """
+
     number_1 = int(input("Enter a number: "))
 
     operator = input("Choose an operator (+, -, *, /) ")
 
     number_2 = int(input("Enter another number: "))  
+
+    """
+    This is an if statement for the different operators
+
+    """
 
     if operator == "+":
         print(number_1, "+", number_2, "=", add(number_1, number_2))
@@ -33,7 +51,10 @@ while another_calculation is True:
     else:
         print("Please enter +, -, * or /")
     
-     
+     """
+     This if statement closes the calculator is the user enters 'no'.
+
+     """
 
     new_calculation = input("Do you want to do another calculation? (yes/no) ")
     if new_calculation == 'no':
