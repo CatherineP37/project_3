8cfca051ffb8e3f0d3798042e8d6b2605b66367d
CatherineP37 <>
2023-10-12 07:18:17 +0000

    Add a while loop

--- run.py
+++ run.py
@@ -10,6 +10,9 @@ def multiply(n1, n2):
 def divide(n1, n2):
     n1 / n2
 
+another_calculation = True
+
+while another_calculation is True:
 
 number_1 = int(input("Enter a number: "))
 
@@ -17,4 +20,6 @@ operator = input("Choose an operator (+, -, *, /) ")
 
 number_2 = int(input("Enter another number: "))
 
-
+new_calculation = input("Do you want to do another calculation? (yes/no) ")
+if new_calculation = "no":
+    another_calculation = False
