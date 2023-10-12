2597c70825d887a5c5db8a3ca97ad880931adcd9
CatherineP37 <>
2023-10-12 10:39:51 +0000

    Fix indentation errors

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
-def add(n1, n2):
-    n1 + n2
-
-def subtract(n1, n2):
-    n1 - n2
-
-def multiply(n1, n2):
-    n1 * n2
-
-def divide(n1, n2):
-    n1 / n2
 
 another_calculation = True
 
 while another_calculation is True:
+    def add(n1, n2):
+        n1 + n2
+
+    def subtract(n1, n2):
+        n1 - n2
+
+    def multiply(n1, n2):
+        n1 * n2
+
+    def divide(n1, n2):
+        n1 / n2
+
 
     number_1 = int(input("Enter a number: "))
 
