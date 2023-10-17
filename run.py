862e381dd2617d951ff3a15388e210bdcc9a7122
CatherineP37 <>
2023-10-17 11:16:48 +0000

    Add if else statement

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
 def calculation():
+    number_1 = int(input("Enter a number: "))
+	operator = int(input("Choose an operator from the following: + - * / "))
+	number_2 = int(input("Enter another number: "))
+
+    if operator == "+":
+		result = number_1 + number_2
+		print(number_1, "+", number_2, "=", result)	
+	elif operator == "-":
+		result = number_1 - number_2
+		print(number_1, "-", number_2, "=", result)		
+	elif operator == "*":
+		result = number_1 * number_2
+		print(number_1, "*", number_2, "=", result)		
+	elif operator == "/":
+		result = number_1 / number_2
+		print(number_1, "/", number_2, "=", result)	
+	else:
+	    print("You have not entered a valid operator, please try again.")
+
+
