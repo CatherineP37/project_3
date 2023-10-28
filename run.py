da66f922cc61ff650c7da6344d3f741b352a0605
CatherineP37 <>
2023-10-28 09:38:49 +0000

    Add record_history functions to the add, subtract, multiply and divide functions

--- run.py
+++ run.py
@@ -21,12 +21,15 @@ def add(n1, n2):
     return n1 + n2
 
 def subtract(n1, n2):
+    record_history(n1 , n2) 
     return n1 - n2
 
 def multiply(n1, n2):
+    record_history(n1 , n2)
     return n1 * n2
 
 def divide(n1, n2):
+    record_history(n1 , n2)
     return n1 / n2
 
 def number_one():
