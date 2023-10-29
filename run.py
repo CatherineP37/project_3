764435bd076a410dc6952c2ad7b79a7f99f39951
CatherineP37 <>
2023-10-29 08:01:28 +0000

    Delete ASCII art

--- run.py
+++ run.py
@@ -10,18 +10,7 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-def number_one():
-    print( 
-        """
-                                                                        
-# #####    #   #       ##### #    # #         # #  ######    # #####  
-#        #  #  #      #    # #    # #       #  #    #   #    # #    # 
-#       #    # #      #      #    # #      #    #   #   #    # #    # 
-#       ###### #      #      #    # #      ######   #   #    # #####  
-#     # #    # #      #    # #    # #      #    #   #   #    # #   #  
- #####  #    # ######  ####   ####  ###### #    #   #    ####  #    # 
- """
-                                                                      )
+def number_one():   
     try:
         global number_1
         number_1 = int(input("Enter a number:\n")) 
